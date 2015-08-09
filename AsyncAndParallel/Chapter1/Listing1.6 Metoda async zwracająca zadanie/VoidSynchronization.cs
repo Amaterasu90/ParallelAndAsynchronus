32635b62c21af57f3272092a9a595ba1a0fd1793
Amaterasu90 <arekpoly@gmail.com>
2015-08-09 13:39:53 +0200

    Add Code From Listing 1.6 Chapter 1

---
+++ AsyncAndParallel/Chapter1/Listing1.6 Metoda async zwracająca zadanie/VoidSynchronization.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndParallel.Chapter1.Listing1._6_Metoda_async_zwracająca_zadanie
+{
+    public class VoidSynchronization
+    {
+        private async Task VoidAction()
+        {
+            printMessage("VoidAction: Początek");
+            long wynik = await AsyncAction("async/await");
+            printMessage("VoidAction: Wynik: " + wynik.ToString());
+            printMessage("VoidAction: Koniec");
+            return;
+        }
+
+        private Task<long> AsyncAction(object argument)
+        {
+            Func<object,long> akcja =
+                (object _argument) =>
+                {
+                    printMessage("Akcja: Początek, argument" + _argument.ToString());
+                    Thread.Sleep(1000);
+                    printMessage("Akcja: Koniec");
+                    return DateTime.Now.Ticks;
+                };
+            Task<long> zadanie = new Task<long>(akcja,argument);
+            zadanie.Start();
+            return zadanie;
+        }
+
+        public async void ProofOfAsyncWork()
+        {
+            printMessage("ProofOfAsyncWork: Początek");
+            await VoidAction();
+            printMessage("ProofOfAsyncWork: Koniec");
+        }
+
+        private void printMessage(string komunikat)
+        {
+            string taskID = Task.CurrentId.HasValue ? Task.CurrentId.ToString() : "UI";
+            Console.WriteLine("! " + komunikat + "( " + taskID + ")");
+        }
+    }
+}
--- AsyncAndParallel/Program.cs
+++ AsyncAndParallel/Program.cs
@@ -3,6 +3,7 @@ using AsyncAndParallel.Chapter1.Listing1._2_Użycie_zadania_do_asynchronicznego_
 using AsyncAndParallel.Chapter1.Listing1._3_Wzór_metody_wykonującej_jakąś_czynność_asynchronicznie;
 using AsyncAndParallel.Chapter1.Listing1._4_Przykład_użycia_modyfikatora_async_i_modyfikatora_await;
 using AsyncAndParallel.Chapter1.Listing1._5_Działanie_modyfikatora_async;
+using AsyncAndParallel.Chapter1.Listing1._6_Metoda_async_zwracająca_zadanie;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace AsyncAndParallel
         {
             ActionByTheAsyncOperator actionByTheAsyncOperator = new ActionByTheAsyncOperator();
             actionByTheAsyncOperator.ProofOfAsyncWork();
+
+            Console.WriteLine("A teraz zmiana. Szpak dziobie bociana.");
+
+            VoidSynchronization voidSynchronization = new VoidSynchronization();
+            voidSynchronization.ProofOfAsyncWork();
 
             Console.ReadKey();
             return 0;
