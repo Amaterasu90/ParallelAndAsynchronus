fb13f86da3f3125ee0b552265c631e78a8f98398
Amaterasu90 <arekpoly@gmail.com>
2015-08-09 14:55:00 +0200

    Add Code From Listing 1.10 Chapter 1

---
+++ AsyncAndParallel/Chapter1/Listion1.10 Obliczenia sekwencyjne/SequenceCalculations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndParallel.Chapter1.Listion1._10_Obliczenia_sekwencyjne
+{
+    public static class SequenceCalculations
+    {
+        private static int rozmiar = 10000;
+
+        public static int Rozmiar
+        {
+            get { return SequenceCalculations.rozmiar; }
+            set { SequenceCalculations.rozmiar = value; }
+        }
+        private static Random r;
+        private static int iloscPowtorzen = 100;
+
+        public static int IloscPowtorzen
+        {
+            private get { return SequenceCalculations.iloscPowtorzen; }
+            set { SequenceCalculations.iloscPowtorzen = value; }
+        }
+
+        public static void run()
+        {
+            r = new Random();
+            double[] tablica = new double[rozmiar];
+            for (int powtorzenia = 0; powtorzenia < rozmiar; powtorzenia++)
+                tablica[powtorzenia] = r.NextDouble();
+
+            double[] wyniki = new double[tablica.Length];
+            int start = System.Environment.TickCount;
+            for (int powtorzenia = 0; powtorzenia < iloscPowtorzen; powtorzenia++)
+                for (int i = 0; i < tablica.Length; i++)
+                    wyniki[i] = AsyncAndParallel.Chapter1.Listing1._9_Metoda_zajmująca_procesor.Math
+                        .obliczenia(tablica[i]);
+            int stop = System.Environment.TickCount;
+            Console.WriteLine("Obliczenia sekwencyjne trwały " + (stop - start).ToString() + " ms.");
+        }
+    }
+}
--- AsyncAndParallel/Program.cs
+++ AsyncAndParallel/Program.cs
@@ -1,16 +1,5 @@
-﻿using AsyncAndParallel.Chapter1;
-using AsyncAndParallel.Chapter1.Listing1._2_Użycie_zadania_do_asynchronicznego_wykonania_kodu;
-using AsyncAndParallel.Chapter1.Listing1._3_Wzór_metody_wykonującej_jakąś_czynność_asynchronicznie;
-using AsyncAndParallel.Chapter1.Listing1._4_Przykład_użycia_modyfikatora_async_i_modyfikatora_await;
-using AsyncAndParallel.Chapter1.Listing1._5_Działanie_modyfikatora_async;
-using AsyncAndParallel.Chapter1.Listing1._6_Metoda_async_zwracająca_zadanie;
-using AsyncAndParallel.Chapter1.Listing1._7_Metoda_async_zwracająca_wartość_long;
-using AsyncAndParallel.Chapter1.Listing1._8_Obsługa_wyjątków_zgłaszanych_przez_metody_async;
+﻿using AsyncAndParallel.Chapter1.Listion1._10_Obliczenia_sekwencyjne;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AsyncAndParallel
 {
@@ -18,8 +7,7 @@ namespace AsyncAndParallel
     {
         public static int Main(string[] args)
         {
-            AsyncExceptions asyncException = new AsyncExceptions();
-            asyncException.ProofOfAsyncWork();
+            SequenceCalculations.run();
 
             Console.ReadKey();
             return 0;
