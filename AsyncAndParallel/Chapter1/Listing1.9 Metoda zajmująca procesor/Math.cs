93579024ab417084f0febd2833c77ead75e20478
Amaterasu90 <arekpoly@gmail.com>
2015-08-09 14:32:30 +0200

    Add Code From Listing 1.9 Chapter 1

---
+++ AsyncAndParallel/Chapter1/Listing1.9 Metoda zajmująca procesor/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndParallel.Chapter1.Listing1._9_Metoda_zajmująca_procesor
+{
+    public static class Math
+    {
+        private static double _x;
+        public static double obliczenia(double x)
+        {
+            for (int i = 0; i < 10; i++)
+                _x = System.Math.Asin(System.Math.Sin(x));
+            return _x;
+        }
+    }
+}
